454a6c42c44e50bc2d4f09298d505d611aba338c
lewyi <68985020+lewyi@users.noreply.github.com>
2026-03-26 20:50:38 +0800

    Update entercookie.py

--- cogs/entercookie.py
+++ cogs/entercookie.py
@@ -8,19 +8,37 @@ class EnterCookie(commands.Cog):
         self.bot = bot
     @app_commands.command(name="输入cookie", description="输入cookie")
     async def entercookie(self, interaction: discord.Interaction):
-        await interaction.response.send_modal(Modalclass())
+        await interaction.response.send_modal(Modalclass(self.bot))
         
 class Modalclass(discord.ui.Modal,title="输入cookie"):
-    name=discord.ui.TextInput(label="请输入Account_Token",style=discord.TextStyle.short,placeholder="Account_Token",required=True)
+    def __init__(self, bot: commands.Bot):
+        super().__init__()
+        self.bot = bot
+
+    CookieID=discord.ui.TextInput(label="请输入Account_Token",style=discord.TextStyle.short,placeholder="Account_Token",required=True)
     async def on_submit(self,interaction:discord.Interaction):
-        with open("data\\user.txt","a") as f:
-            encode=base64.b64encode(self.name.value.encode()).decode()
-            if any(encode in line for line in open("data\\user.txt")):
-                await interaction.response.send_message("⚠ 已存在相同的Cookie",ephemeral=True)
-                return
-            f.write(f"{interaction.user.id}:{encode}\n")
-            await interaction.response.send_message("✅ 已保存Cookie",ephemeral=True)
-    
-    
+        encode=base64.b64encode(self.CookieID.value.encode()).decode()
+        UID=str(interaction.user.id)
+        cursor=await self.bot.db.execute("SELECT COUNT(*) FROM User WHERE DiscordID=? and Cookie=?", (UID, encode))
+        number=await cursor.fetchone()
+        await cursor.close()
+        if number[0]>0:
+            embed=discord.Embed(
+                title="错误",
+                description=f"数据库已有该Cookie记录", 
+                color=discord.Color.random(),
+            )
+            await interaction.response.send_message(embed=embed,ephemeral=True)
+        else:
+            await self.bot.db.execute("INSERT INTO User(DiscordID,Cookie) VALUES (?,?)",(str(UID),encode))
+            await self.bot.db.commit()
+            embed=discord.Embed(
+                title="成功",
+                description=f"Cookie已保存", 
+                color=discord.Color.random(),
+            )
+            await interaction.response.send_message(embed=embed,ephemeral=True)
+            print (f"用户 {UID} 已绑定 Cookie")
+
 async def setup(bot):
     await bot.add_cog(EnterCookie(bot))
